566ffc82a0360a7f3a382c4d2724dcb7a384ba4f
h0use <denis@mysale.com>
2022-06-14 22:26:01 +1000

    Add basics

---
+++ src/StarCitizenApi/Launcher.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NSW.StarCitizen.API
+{
+    public class Launcher
+    {
+        public const string DefaultFolderName = "RSI Launcher";
+        public string FolderPath { get; init; }
+        public Launcher(string folderPath)
+        {
+            FolderPath = folderPath;
+        }
+    }
+}
---
+++ src/StarCitizenApi/StarCitizenAPI.cs
@@ -0,0 +1,23 @@
+﻿namespace NSW.StarCitizen.API
+{
+    public class StarCitizenAPI
+    {
+        public string LibraryPath { get; init; }    
+
+        public Launcher Launcher { get; init; }  
+
+        public StarCitizenAPI(string libraryPath)
+        {
+            if(!Directory.Exists(libraryPath))
+                throw new ArgumentException("Incorrect folder name", nameof(libraryPath));
+
+            LibraryPath = libraryPath;
+            Launcher = GetLauncher();
+        }
+
+        public Launcher GetLauncher(string? launcherFolder = null)
+        {
+            return new Launcher(Path.Combine(LibraryPath, launcherFolder ?? Launcher.DefaultFolderName));
+        }
+    }
+}
